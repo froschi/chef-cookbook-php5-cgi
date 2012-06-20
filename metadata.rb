39a77dc9582e93a914b63d22081bb679d2868111
Thorsten Fischer <thorsten@froschi.org>
2012-06-20 14:50:58 +0200

    Added dependencies, for use on lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,17 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libbz2", ">= 0.0.1"
+depends          "libcomerr", ">= 0.0.1"
+depends          "libdb", ">= 0.0.1"
+depends          "libgssapi-krb5", ">= 0.0.1"
+depends          "libkrb5", ">= 0.0.1"
+depends          "libmagic", ">= 0.0.1"
+depends          "libpcre", ">= 0.0.1"
+depends          "libssl", ">= 0.0.1"
+depends          "libxml2", ">= 0.0.1"
+depends          "mime-support", ">= 0.0.1"
+depends          "php5-common", ">= 0.0.1"
+depends          "tzdata", ">= 0.0.1"
+depends          "ucf", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,22 @@
+include_recipe "libbz2"
+include_recipe "libdb"
+include_recipe "libmagic"
+include_recipe "libpcre"
+include_recipe "libssl"
+include_recipe "libxml2"
+include_recipe "mime-support"
+include_recipe "php5-common"
+include_recipe "tzdata"
+include_recipe "ucf"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libcomerr"
+  include_recipe "libgssapi-krb5"
+  include_recipe "libkrb5"
+
   packages |= %w/
     php5-cgi
   /
